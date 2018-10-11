f1f7e0662c9cc4f3fc4f5083b4d3ef8fdfa5b0e7
L.Answer <answer_ljm@163.com>
2018-10-11 16:40:46 +0800

    answer

--- src/main/java/com/answer/mybatis/generator/dao/RiskScoreMapper.java
+++
@@ -1,9 +0,0 @@
-package com.answer.mybatis.generator.dao;
-
-import com.answer.mybatis.generator.entity.RiskScore;
-
-public interface RiskScoreMapper {
-    int insert(RiskScore record);
-
-    int insertSelective(RiskScore record);
-}
